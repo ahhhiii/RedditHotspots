10d9abc71dde88eac04f47ba77f4e0247991c403
Jaffaaaa <39811887+Jaffaaaa@users.noreply.github.com>
2020-12-05 15:58:41 +0000

    Query start

--- src/com/ahammond/reddithotspots/Core.java
+++ src/com/ahammond/reddithotspots/Core.java
@@ -1,18 +1,48 @@
 package com.ahammond.reddithotspots;
 
+import com.ahammond.reddithotspots.display.Display;
+import com.ahammond.reddithotspots.redditapi.Reddit;
+import com.ahammond.reddithotspots.redditapi.RedditPost;
+
+import java.io.IOException;
+
 public class Core {
 
-    public Core() {
-        initReddit();
-        initDisplay();
+    public Display display = new Display(this);
+    public Reddit reddit = new Reddit(this);
+
+    public void query(String subreddit) {
+        alertRunning();
+        RedditPost[] posts = getPosts(subreddit);
+
+        if (posts == null) {
+            return;
+        }
+
+        alertTabling();
     }
 
-    private void initDisplay() {
-        
+    private RedditPost[] getPosts(String subreddit) {
+        RedditPost[] posts;
+        try {
+            posts = reddit.getTopPosts(100, subreddit);
+        } catch (IOException e) {
+            display.search.setText("Failed.");
+            display.search.setEnabled(true);
+            e.printStackTrace();
+            return null;
+        }
+
+        return posts;
     }
 
-    private void initReddit() {
+    private void alertRunning() {
+        display.search.setEnabled(false);
+        display.alert("Querying reddit...");
+    }
 
+    private void alertTabling() {
+        display.alert("Tabling posts...");
     }
 
 }
